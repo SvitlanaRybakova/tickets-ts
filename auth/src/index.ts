17fd4b48dd7bb9f7a6440bd34d53e19250d573ae
Svitlana Rybakova <svitlana.rybakova@desifer.se>
2023-03-13 15:35:02 +0100

    implemented the jwt-token

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import mongoose from 'mongoose';
+import CookieSession from 'cookie-session';
 
 import { currentuserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -13,7 +14,16 @@ import { NotFoundError } from './errors/not-found-error';
 const PORT = 3000;
 
 const app = express();
+
+app.set('trust proxy', true);
 app.use(json());
+
+app.use(
+  CookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
 app.use(currentuserRouter);
 app.use(signinRouter);
@@ -27,9 +37,12 @@ app.all('*', async () => {
 app.use(errorHandler);
 
 const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
   try {
     await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
-   } catch (err) {
+  } catch (err) {
     console.error('Cannot connect to the db', err);
   }
 };
