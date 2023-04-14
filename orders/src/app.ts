fb6fefed32a774a12b1c42e991a8840d5cc1f0ba
Svitlana Rybakova <rsv0011@gmail.com>
2023-04-14 18:41:33 +0200

    init the orders feature

---
+++ orders/src/app.ts
@@ -0,0 +1,38 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import CookieSession from 'cookie-session';
+import { errorHandler, NotFoundError, currentUser } from '@sviry/common';
+
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes/index';
+import { updateTicketRouter } from './routes/update';
+
+const PORT = 3000;
+
+const app = express();
+
+app.set('trust proxy', true);
+app.use(json());
+
+app.use(
+  CookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+
+app.use(currentUser);
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
+
+app.all('*', async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app, PORT };
